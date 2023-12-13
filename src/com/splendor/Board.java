5a6baebc309662ace841ea7db2c8e95170e08f99
BD56 <153074465+BD56@users.noreply.github.com>
2023-12-13 09:50:59 +0100

    Add files via upload

--- src/com/splendor/Board.java
+++ src/com/splendor/Board.java
@@ -1,82 +1,134 @@
-package com.splendor;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.MessageFormat;
 import java.util.Stack;
-
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Collections;
 
-
 public class Board implements Displayable {
 
-    /**
-     * Add remaining cards and next tier to the deck representation
-     * and return it.
-     * 
-     * @param tier The tier of the next available card in the deck.
-     * @return The string array representation of the deck.
-     */
-    public String[] deckToStringArray(int tier) {
-        int remainingCards = 0; // TODO
-        String cards = String.format("%02d", remainingCards);
-        String plural = remainingCards > 1 ? "s" : "";
-        // Join and format the deck preview with the corresponding values.
-        String deckPreview = String.join("\n", Constants.DECK_PREVIEW);
-        deckPreview = MessageFormat.format(deckPreview, cards, plural, tier);
-        return deckPreview.split("\n");
+    /* --- Stringers --- */
+    
+    private HashMap<Resource, Integer> resources = new HashMap<>();
+    private List<Stack<DevCard>> stackCards; // Piles de cartes pour chaque niveau
+    private DevCard[][] visibleCards; // Cartes visibles organisées par niveau et position
+    
+    /* --- Méthodes de gestion des ressources --- */
+    public int getNbResource(Resource type) {
+        return resources.getOrDefault(type, 0);
     }
 
-    private String[] resourcesToStringArray() {
+    public void setNbResource(Resource type, int nb) {
+        resources.put(type, nb);
+    }
+
+    public void updateNbResource(Resource type, int v) {
+        int currentNb = getNbResource(type);
+        resources.put(type, Math.max(0, currentNb + v));
+    }
+
+    public Set<Resource> getAvailableResources() {
+        return resources.keySet();
+    }
+
+    /* --- Méthodes de gestion des cartes --- */
+    public DevCard getCard(int tier, int column) {
+        return visibleCards[tier][column];
+    }
+
+    public void updateCard(DevCard card, int tier, int column) {
+        if (!stackCards.get(tier).isEmpty()) {
+            visibleCards[tier][column] = stackCards.get(tier).pop();
+        } else {
+            visibleCards[tier][column] = null;
+        }
+    }
+
+    public DevCard drawCard(int tier) {
+        if (!stackCards.get(tier).isEmpty()) {
+            return stackCards.get(tier).pop();
+        }
+        return null;
+    }
+
+    public boolean canGiveSameTokens(Resource resource) {
+        return getNbResource(resource) >= 4;
+    }
+
+    public boolean canGiveDiffTokens(List<Resource> resourcesList) {
+        for (Resource res : resourcesList) {
+            if (getNbResource(res) < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String[] deckToStringArray(int tier){
+        /** EXAMPLE
+         * ┌────────┐
+         * │        │╲ 
+         * │ reste: │ │
+         * │   16   │ │
+         * │ cartes │ │
+         * │ tier 3 │ │
+         * │        │ │
+         * └────────┘ │
+         *  ╲________╲│
+         */
+        int nbCards = stackCards.get(tier).size(); // Remplacé par le nombre de cartes dans le deck pour le niveau donné
+        String[] deckStr = {"\u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510  ",
+                            "\u2502        \u2502\u2572 ",
+                            "\u2502 reste: \u2502 \u2502",
+                            "\u2502   "+String.format("%02d", nbCards)+"   \u2502 \u2502",
+                            "\u2502 carte"+(nbCards>1 ? "s" : " ")+" \u2502 \u2502",
+                            "\u2502 tier "+tier+" \u2502 \u2502",
+                            "\u2502        \u2502 \u2502",
+                            "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \u2502",
+                            " \u2572________\u2572\u2502"};
+        return deckStr;
+    }
+
+    private String[] resourcesToStringArray(){
         /** EXAMPLE
          * Resources disponibles : 4♥R 4♣E 4♠S 4♦D 4●O
          */
         String[] resStr = {"Resources disponibles : "};
-        /*
-         * A decommenter
-        for(ACOMPLETER){ //-- parcourir l'ensemble des resources (res) en utilisant l'énumération Resource
-            resStr[0] += resources.getNbResource(res)+res.toSymbol()+" ";
+        for (Resource res : resources.keySet()) {
+            resStr[0] += getNbResource(res) + res.toSymbol() + " ";
         }
-                 */
-        resStr[0] += "        ";
         return resStr;
     }
 
-    private String[] boardToStringArray() {
+    private String[] boardToStringArray(){
         String[] res = Display.emptyStringArray(0, 0);
-        /*
-         * 
-
-        //Deck display
+        
+        // Deck display
         String[] deckDisplay = Display.emptyStringArray(0, 0);
-        for(int i=stackCards.size();i>0;i--){
+        for (int i = stackCards.size(); i > 0; i--) {
             deckDisplay = Display.concatStringArray(deckDisplay, deckToStringArray(i), true);
         }
-
-        //Card display
+    
+        // Card display
         String[] cardDisplay = Display.emptyStringArray(0, 0);
-        for(ACOMPLETER){ //-- parcourir les différents niveaux de carte (i)
+        for (int i = 0; i < visibleCards.length; i++) { // parcourir les différents niveaux de carte (i)
             String[] tierCardsDisplay = Display.emptyStringArray(8, 0);
-            for(ACOMPLETER){ //-- parcourir les 4 cartes faces visibles pour un niveau donné (j)
-                tierCardsDisplay = Display.concatStringArray(tierCardsDisplay, visibleCards[i][j]!=null ? visibleCards[i][j].toStringArray() : DevCard.noCardStringArray(), false);
+            for (int j = 0; j < visibleCards[i].length; j++) { // parcourir les 4 cartes faces visibles pour un niveau donné (j)
+                tierCardsDisplay = Display.concatStringArray(tierCardsDisplay, visibleCards[i][j] != null ? visibleCards[i][j].toStringArray() : DevCard.noCardStringArray(), false);
             }
             cardDisplay = Display.concatStringArray(cardDisplay, Display.emptyStringArray(1, 40), true);
             cardDisplay = Display.concatStringArray(cardDisplay, tierCardsDisplay, true);
         }
-        
+    
         res = Display.concatStringArray(deckDisplay, cardDisplay, false);
         res = Display.concatStringArray(res, Display.emptyStringArray(1, 52), true);
         res = Display.concatStringArray(res, resourcesToStringArray(), true);
         res = Display.concatStringArray(res, Display.emptyStringArray(35, 1, " \u250A"), false);
         res = Display.concatStringArray(res, Display.emptyStringArray(1, 54, "\u2509"), true);
-                 */
+    
         return res;
     }
 
