74ab1999cd029581b9b258739bec0c95fc5bae79
maximedrean <153073978+maximedrean@users.noreply.github.com>
2023-12-13 10:00:32 +0100

    Add files via upload

---
+++ test/com/splendor/PlayerTest.java
@@ -0,0 +1,18 @@
+package com.splendor;
+
+import com.splendor.player.HumanPlayer;
+import com.splendor.player.Player;
+
+public class PlayerTest {
+
+    public static void main(String[] args) {
+        Player player = new HumanPlayer("Camille", 0);
+
+        Resources cost = new Resources();
+        cost.setNbResource(Resource.DIAMOND, 2);
+        cost.setNbResource(Resource.EMERALD, 1);
+        cost.setNbResource(Resource.SAPPHIRE, 3);
+        DevCard card = new DevCard(1, cost, 2, Resource.DIAMOND);
+        System.out.println(player.canBuyCard(card));
+    }
+}
