3a951a7794cf1abdd11d56883baf63ebd4728f8c
Maxime <maximedrean@orange.fr>
2024-01-09 16:16:17 +0100

    feat: changed resources paths for BlueJ

--- src/main/java/com/splendor/constants/Project.java
+++ src/main/java/com/splendor/constants/Project.java
@@ -9,8 +9,10 @@ public final class Project {
      * The file path of the CSV file containing development card statistics.
      */
     public static final String CARD_STATISTIC_FILE =
-        Paths.get("build", "resources", "main", "stats.csv").toString();
+        Paths.get("..", "resources", "stats.csv").toString(); // BlueJ.
+        // Paths.get("build", "resources", "main", "stats.csv").toString(); // Gradle.
 
     public static final String DISPLAY_FONT_FILE =
-        Paths.get("build", "resources", "main", "unifont.otf").toString();
+        Paths.get("..", "resources", "unifont.otf").toString(); // BlueJ.
+        // Paths.get("build", "resources", "main", "unifont.otf").toString(); // Gradle.
 }
